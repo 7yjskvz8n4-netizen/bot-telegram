83a99a33433671e1f5afea34a9af78c468586a95
7yjskvz8n4-netizen <7yjskvz8n4@privaterelay.appleid.com>
2026-05-08 22:29:37 +0200

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -5,7 +5,7 @@ ODDS_API_KEY = "TU_API_KEY"
 url = "https://api.the-odds-api.com/v4/sports"
 
 params = {
-    "90ae2f6d7b5ddcd76926f1cf40be2ad7": ODDS_API_KEY
+    "167721723854a65832f09abdeb92952b": ODDS_API_KEY
 }
 
 response = requests.get(url, params=params)
