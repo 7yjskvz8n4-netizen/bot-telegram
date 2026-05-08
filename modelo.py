f80c74034a3bb26c3767485835019285dd68b9e0
7yjskvz8n4-netizen <7yjskvz8n4@privaterelay.appleid.com>
2026-05-08 09:59:40 +0200

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -1,8 +1,16 @@
 import requests
 import time
+import math
 
 TOKEN = "8510764547:AAHFpJ1_aPFdDDIYjVptLbxNgUAQh-dat7o"
 CHAT_ID = "1335805552"
+ODDS_API_KEY = "8c45ed3a66d6870a222bce3c47a34a88"
+
+BANK = 1000
+
+# =========================
+# TELEGRAM
+# =========================
 
 def send(msg):
 
@@ -13,12 +21,42 @@ def send(msg):
         "text": msg
     })
 
-print("🔥 BOT ARRANCADO")
 
-while True:
+# =========================
+# POISSON SIMPLE
+# =========================
 
-    print("📩 enviando mensaje")
+def poisson(k, lam):
 
-    send("✅ Bot funcionando")
+    return (lam ** k * math.exp(-lam)) / math.factorial(k)
 
-    time.sleep(60)
+
+# =========================
+# PROBABILIDAD SIMPLE
+# =========================
+
+def model_prob():
+
+    home_goals = 1.4
+    away_goals = 1.1
+
+    prob = 0
+
+    for i in range(6):
+        for j in range(6):
+
+            p = poisson(i, home_goals) * poisson(j, away_goals)
+
+            if i > j:
+                prob += p
+
+    return prob
+
+
+# =========================
+# SCANNER
+# =========================
+
+def scan():
+
+    url = "https://api.the-odds-api.com/v4/sports/soccer_spain_la_liga/od
