195eec85368bcfcc6eaf8035f7420982fe0ff26b
7yjskvz8n4-netizen <7yjskvz8n4@privaterelay.appleid.com>
2026-05-08 09:54:03 +0200

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -1,4 +1,24 @@
-print("🔥 BOT FUNCIONANDO")
+import requests
+import time
+
+TOKEN = "8510764547:AAHFpJ1_aPFdDDIYjVptLbxNgUAQh-dat7o"
+CHAT_ID = "1335805552"
+
+def send(msg):
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": msg
+    })
+
+print("🔥 BOT ARRANCADO")
 
 while True:
-    print("✅ sigue vivo")
+
+    print("📩 enviando mensaje")
+
+    send("✅ Bot funcionando")
+
+    time.sleep(60)
